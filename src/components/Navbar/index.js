20987a34d2156c9bd95d1f55c4f5831e4d5bc1dc
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-01-31 23:55:39 +0300

    Added hamburger menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/images/logo1.png";
+import Logo from "../../assets/images/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   const links = [
     {
       id: 1,
@@ -30,20 +36,33 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="nav">
-      <div className="nav__logo">
+    <div className="nav__mobile">
+      <div className="nav__logo mobile">
         <Link to="/">
           <img src={Logo} alt="" />
         </Link>
       </div>
-      <ul className="nav__list">
-        {links?.map((link) => (
-          <li key={link?.id} className="nav__link">
-            <Link to={link?.path}>{link?.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      <nav ref={navRef} className="nav">
+        <div className="nav__logo">
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+        </div>
+        <ul className="nav__list">
+          {links?.map((link) => (
+            <li onClick={showNavbar} key={link?.id} className="nav__link">
+              <Link to={link?.path}>{link?.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <button className="nav__btn nav__closebtn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav__btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </div>
   );
 };
 
