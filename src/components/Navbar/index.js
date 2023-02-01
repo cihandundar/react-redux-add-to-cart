51bb3706e61fdb972235149a71c53e4449286004
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-01 20:10:37 +0300

    navbar link fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -21,16 +21,6 @@ const Navbar = () => {
     },
     {
       id: 3,
-      name: "About Us",
-      path: "/about",
-    },
-    {
-      id: 4,
-      name: "Contact Us",
-      path: "/contact",
-    },
-    {
-      id: 5,
       name: "Login / Register",
       path: "/login",
     },
