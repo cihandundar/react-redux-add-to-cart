ca5ccbc935b1caa13ab414f3f7c0f03fcc5d1c70
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-02 13:16:49 +0300

    footer component edited

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import GithubIcon from "assets/icons/GıthubIcon";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__left">
+          <p>© 2023 Enride(22)</p>
+        </div>
+        <div className="footer__icon">
+          <a
+            href="https://github.com/cihandundar/react-redux-add-to-cart"
+            target={"_blank"}
+          >
+            <GithubIcon />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
