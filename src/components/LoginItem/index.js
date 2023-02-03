12a52ab733d85cae3dbea6abe417bf4c0870d541
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-03 21:34:46 +0300

    Added login page and component

---
+++ src/components/LoginItem/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginItem = () => {
+  return <div>LoginItem</div>;
+};
+
+export default LoginItem;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,12 @@
+import LoginItem from "components/LoginItem";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <LoginItem />
+    </div>
+  );
 };
 
 export default Login;
