ac3d8f0b97c7b78df1ec84a36f12799a90eb5de9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-04-06 18:24:06 +0300

    loading spinner added

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -4,9 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchProduct } from "../../redux/productSlice";
 import { add } from "redux/cartSlice";
+import Loading from "../../assets/images/loading.gif";
 const ProductItem = () => {
   const data = useSelector((state) => state?.productReducer?.data);
   const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state?.productReducer?.isLoading);
+
   useEffect(() => {
     dispatch(fetchProduct());
   }, [dispatch]);
@@ -20,26 +23,32 @@ const ProductItem = () => {
     <div className="item">
       <div className="item__container">
         <div className="item__wrapper">
-          {data?.products?.map((item) => (
-            <div className="item__card" key={item?.id}>
-              <Link to={`/products/${item?.slug}`}>
-                <div className="item__card__img">
-                  <img src={item?.coverImage} alt="" />
-                </div>
-              </Link>
-              <div className="item__card__title">
-                <h3>{item?.title}</h3>
-                <span>{item?.name}</span>
-              </div>
-              <div className="item__card__price">
-                {item.wasPriceRange.toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                })}
-              </div>
-              <button onClick={() => handleAdd(item)}>Add to Cart</button>
+          {isLoading ? (
+            <div>
+              <img src={Loading} alt="" />
             </div>
-          ))}
+          ) : (
+            data?.products?.map((item) => (
+              <div className="item__card" key={item?.id}>
+                <Link to={`/products/${item?.slug}`}>
+                  <div className="item__card__img">
+                    <img src={item?.coverImage} alt="" />
+                  </div>
+                </Link>
+                <div className="item__card__title">
+                  <h3>{item?.title}</h3>
+                  <span>{item?.name}</span>
+                </div>
+                <div className="item__card__price">
+                  {item.wasPriceRange.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </div>
+                <button onClick={() => handleAdd(item)}>Add to Cart</button>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,35 +1,44 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { remove } from "redux/cartSlice";
-
+import Loading from "../../assets/images/loading.gif";
 const Cart = () => {
   const data = useSelector((state) => state?.cartReducer);
   console.log(data);
+  const isLoading = useSelector((state) => state?.productReducer?.isLoading);
   const dispatch = useDispatch();
 
   return (
     <div className="cart">
       <div className="cart__container">
         <div className="cart__wrapper">
-          {data?.map((item) => (
-            <div className="cart__box" key={item?.id}>
-              <div className="cart__box__img">
-                <img src={item?.coverImage} alt="" />
-              </div>
-
-              <div className="cart__box__title">
-                <h3>{item?.title}</h3>
-                <span>{item?.name}</span>
-              </div>
-              <div className="cart__box__price">
-                {item.wasPriceRange.toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                })}
-              </div>
-              <button onClick={() => dispatch(remove(item.id))}>Remove</button>
+          {isLoading ? (
+            <div>
+              <img src={Loading} alt="" />
             </div>
-          ))}
+          ) : (
+            data?.map((item) => (
+              <div className="cart__box" key={item?.id}>
+                <div className="cart__box__img">
+                  <img src={item?.coverImage} alt="" />
+                </div>
+
+                <div className="cart__box__title">
+                  <h3>{item?.title}</h3>
+                  <span>{item?.name}</span>
+                </div>
+                <div className="cart__box__price">
+                  {item.wasPriceRange.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </div>
+                <button onClick={() => dispatch(remove(item.id))}>
+                  Remove
+                </button>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
