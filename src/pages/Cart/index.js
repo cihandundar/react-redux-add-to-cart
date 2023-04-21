8762f3fd01e79db92bf594da75223b70f34b7f17
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-04-21 12:02:42 +0300

    cart page fixed

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,36 +1,99 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { remove } from "redux/cartSlice";
-
+import { Link } from "react-router-dom";
+import { addToCart, clearCart, decreaseCart, remove } from "redux/cartSlice";
+import Empty from "../../assets/images/empty.jpg";
 const Cart = () => {
   const data = useSelector((state) => state?.cartReducer);
   console.log(data);
   const dispatch = useDispatch();
 
+  const handleRemove = (item) => {
+    dispatch(remove(item));
+  };
+
+  const handleDecreaseCart = (item) => {
+    dispatch(decreaseCart(item));
+  };
+
+  const handleIncreaseCart = (item) => {
+    dispatch(addToCart(item));
+  };
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <div className="cart">
       <div className="cart__container">
-        <div className="cart__wrapper">
-          {data?.map((item) => (
-            <div className="cart__box" key={item?.id}>
-              <div className="cart__box__img">
-                <img src={item?.coverImage} alt="" />
-              </div>
-
-              <div className="cart__box__title">
-                <h3>{item?.title}</h3>
-                <span>{item?.name}</span>
-              </div>
-              <div className="cart__box__price">
-                {item.wasPriceRange.toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                })}
-              </div>
-              <button onClick={() => dispatch(remove(item.id))}>Remove</button>
+        {data.cartItems.length === 0 ? (
+          <div className="cart__empty">
+            <h3>Your cart is currently empty</h3>
+            <div className="cart__shopping">
+              <Link to="/product">
+                <span>Start Shopping</span>
+              </Link>
             </div>
-          ))}
-        </div>
+            <div className="cart__empty__img">
+              <img src={Empty} alt="" />
+            </div>
+          </div>
+        ) : (
+          <div className="cart__wrapper">
+            <h2>Shopping Cart</h2>
+            <div className="cart__wrapper__title">
+              <h3>Product</h3>
+              <h3>Price</h3>
+              <h3>Quantity</h3>
+              <h3>Total</h3>
+            </div>
+            {data?.cartItems?.map((item) => (
+              <div className="cart__box" key={item?.id}>
+                <div className="cart__box__img">
+                  <img src={item?.coverImage} alt="" />
+                  <div className="cart__box__title">
+                    <h3>{item?.title}</h3>
+                    <span>{item?.name}</span>
+                    <button onClick={() => dispatch(handleRemove(item))}>
+                      Remove
+                    </button>
+                  </div>
+                </div>
+                <div className="cart__box__price">
+                  {item.wasPriceRange?.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </div>
+                <div className="cart__box__quantity">
+                  <button onClick={() => handleDecreaseCart(item)}>-</button>
+                  <div className="count">{item.cartQuantity}</div>
+                  <button onClick={() => handleIncreaseCart(item)}>+</button>
+                </div>
+                <div className="cart__box__total">
+                  ${item.wasPriceRange * item.cartQuantity}
+                </div>
+              </div>
+            ))}
+            <div className="cart__box__summary">
+              <div className="cart__box__checkout">
+                <div className="cart__box__clear">
+                  <button onClick={() => handleClearCart()}>Clear Cart</button>
+                </div>
+                <div className="subtotal">
+                  <div>
+                    <span>Subtotal</span>
+                    <span>${data.cartTotalAmount}</span>
+                  </div>
+                  <Link to="/login">
+                    <button>Check out</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
